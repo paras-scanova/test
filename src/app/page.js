b33593ea12338d87aff471265ae30e0731abd7ab
Paras <paras.sindhwal@scnaova.io>
2023-04-26 10:40:18 +0530

    add: p tag

--- src/app/page.js
+++ src/app/page.js
@@ -49,7 +49,8 @@ export default function Home() {
           <h2>
             Docs <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>Find in-depth information about Next.js and React.js features and API.</p>
+          <p>Find in-depth information about Next.js and React.js features and API.</p>
         </a>
 
         <a
